498ebacc15ba9f0b2e4988b13c9badf8a123d947
mhmdsalah <ms.mhmdsalah@gmail.com>
2025-10-26 09:47:22 +0300

    -

--- src/components/shared/suggested/Suggested.tsx
+++ src/components/shared/suggested/Suggested.tsx
@@ -8,6 +8,8 @@ import Slider from "react-slick";
 import Product from "../product/Product";
 import type { TSimpleProduct } from "../../../store/cart/cartSlice";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 const Suggested = () => {
   const { t } = useTranslation();
   const { data, isLoading } = useQuery({
@@ -17,38 +19,43 @@ const Suggested = () => {
       return res?.data;
     },
   });
+
   const products = data?.products ?? [];
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 800,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    pauseOnHover: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
+  // const settings = {
+  //   dots: false,
+  //   adaptiveHeight: true,
+  //   infinite: true,
+  //   speed: 800,
+  //   slidesToShow: 5,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  //   arrows: false,
+  //   pauseOnHover: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
 
   return (
     <Section className="suggested">
@@ -56,17 +63,41 @@ const Suggested = () => {
       {isLoading ? (
         "Products Loading.."
       ) : (
-        <Slider {...settings}>
+        // <Slider {...settings}>
+
+        //   {products.map((p: TSimpleProduct) => (
+        //     <Product
+        //       id={p.id}
+        //       title={p.title}
+        //       img={p.thumbnail}
+        //       price={p.price}
+        //       category={p.category}
+        //     />
+        //   ))}
+        // </Slider>
+
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={5}
+          breakpoints={{
+            1024: { slidesPerView: 5 },
+            768: { slidesPerView: 3 },
+            480: { slidesPerView: 1 },
+          }}
+          autoplay={{ delay: 3000 }}
+        >
           {products.map((p: TSimpleProduct) => (
-            <Product
-              id={p.id}
-              title={p.title}
-              img={p.thumbnail}
-              price={p.price}
-              category={p.category}
-            />
+            <SwiperSlide key={p.id}>
+              <Product
+                id={p.id}
+                title={p.title}
+                img={p.thumbnail}
+                price={p.price}
+                category={p.category}
+              />
+            </SwiperSlide>
           ))}
-        </Slider>
+        </Swiper>
       )}
     </Section>
   );
